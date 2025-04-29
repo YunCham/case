20f21d8d0229dcb4fd309da98a8829d1dc657172
yuncham <yuncham2.0@gmail.com>
2025-04-29 10:41:45 -0400

    reglas

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,12 +9,18 @@ const config = {
   ],
   "extends": [
     "next/core-web-vitals",
+    'plugin:@typescript-eslint/recommended',
     "plugin:@typescript-eslint/recommended-type-checked",
     "plugin:@typescript-eslint/stylistic-type-checked"
   ],
   "rules": {
     "@typescript-eslint/array-type": "off",
     "@typescript-eslint/consistent-type-definitions": "off",
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unsafe-assignment': 'off',
+    '@typescript-eslint/no-unsafe-call': 'off',
+    '@typescript-eslint/no-unsafe-member-access': 'off',
+    '@typescript-eslint/prefer-nullish-coalescing': 'off',
     "@typescript-eslint/consistent-type-imports": [
       "warn",
       {
--- next.config.js
+++ next.config.js
@@ -5,7 +5,10 @@
 import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
-const config = {
+const config = { 
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   async redirects() {
     return [
       {
